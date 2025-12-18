4d6b8ae9d057056e56c32a663eda19f9bcf83e79
Oleksandr <oleksandrpc@air-oleksandr.home>
2025-12-18 15:33:17 +0100

    chore(backend): bootstrap spring boot

---
+++ backend/src/main/java/com/citybus/tracker/CitybusTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.citybus.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitybusTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitybusTrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/citybus/tracker/CitybusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citybus.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitybusTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
